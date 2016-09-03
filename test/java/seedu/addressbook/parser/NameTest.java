cbe8b316e82c43fa00342cd51bca93a66b19465d
Edmund <edmundmok@outlook.com>
2016-09-03 17:37:21 +0800

    Add a NameTest for testing isSimilar() method of Name class

---
+++ test/java/seedu/addressbook/parser/NameTest.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.parser;
+
+import seedu.addressbook.data.person.Name;
+
+public class NameTest {
+
+	/*
+	 * Tests for the isSimilar() method for the Name class
+	 */
+	
+}
