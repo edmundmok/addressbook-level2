9c98fbbf88dcb3534a6d3c33e7cc38d922c58680
Edmund <edmundmok@outlook.com>
2016-09-03 18:22:34 +0800

    Add first test to test for null case

--- test/java/seedu/addressbook/parser/NameTest.java
+++ test/java/seedu/addressbook/parser/NameTest.java
@@ -1,11 +1,29 @@
 package seedu.addressbook.parser;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Name;
 
 public class NameTest {
+	
+	private Name name;
+	
+	@Before
+	public void setup(){
+		try {
+			name = new Name("John Smith");
+		} catch (IllegalValueException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/*
 	 * Tests for the isSimilar() method for the Name class
 	 */
-	
+	@Test
+	public void isSimilar_nullOther_returnFalse(){
+		assertFalse(name.isSimilar(null));
+	}
 }
