b3e9b961973f3ffd18e093c19a9df11ce19239b6
Edmund <edmundmok@outlook.com>
2016-09-03 20:54:23 +0800

    Add new association class Tagging

---
+++ src/seedu/addressbook/data/Tagging.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.data;
+
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.tag.Tag;
+
+public class Tagging {
+	
+	enum TaggingType {
+		ADD_TAG, REMOVE_TAG;
+	}
+	
+	private TaggingType taggingType;
+	private Person person;
+	private Tag tag;
+	
+	public Tagging(TaggingType taggingType, Person person, Tag tag){
+		this.taggingType = taggingType;
+		this.person = person;
+		this.tag = tag;
+	}
+	
+	public TaggingType getTaggingType(){
+		return taggingType;
+	}
+	
+	public Person getPerson(){
+		return person;
+	}
+	
+	public Tag getTag(){
+		return tag;
+	}
+}
